c9a4bdcc1e56c389d4ecc41582be1949e842a84d
LLuuDev <lluudev@gmail.com>
2024-08-08 19:41:30 +0900

    setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [like, setLike] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>{like}</h1>
+        <button onClick={() => {
+          setLike(like + 1);
+        }}>
+          좋아요
+        </button>
+      </div>
   );
 }
 
